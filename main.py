b2f9e789c43b499e3d2845bb19a2e357c3a917a6
Kriiiisambr <kriiiisambr@gmail.com>
2025-04-05 19:09:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import asyncio
 from telegram import Bot
 from datetime import datetime
 
@@ -6,5 +7,8 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN")
 CHAT_ID = os.environ.get("CHAT_ID")
 bot = Bot(token=BOT_TOKEN)
 
-now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-bot.send_message(chat_id=CHAT_ID, text=f"📨 Тест: бот может писать! Сейчас {now}")
+async def send_message():
+    now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    await bot.send_message(chat_id=CHAT_ID, text=f"📨 Тест: бот может писать! Сейчас {now}")
+
+asyncio.run(send_message())
